c841cd2f68a8ed28302ee52353b7d3f0d12acab1
nuker <rinards.sondors@gmail.com>
2023-04-25 20:44:54 +0100

    implement struct for shapes

--- models/shapes.go
+++ models/shapes.go
@@ -1,5 +1,14 @@
 package models
 
-func Perimeter(width, height float64) float64 {
-	return (width + height) * 2
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func Perimeter(rect Rectangle) float64 {
+	return (rect.Width + rect.Height) * 2
+}
+
+func Area(area Rectangle) float64 {
+	return area.Width * area.Height
 }
