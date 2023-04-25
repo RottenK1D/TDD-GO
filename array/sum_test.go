4876b3bd801274d89e1a15ae43d32b3a90027001
nuker <rinards.sondors@gmail.com>
2023-04-25 19:58:47 +0100

    refactor, implement checkSums func

--- array/sum_test.go
+++ array/sum_test.go
@@ -27,32 +27,37 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given %v", got, want, numbers)
 		}
 	})
+}
 
+func TestSumAll(t *testing.T) {
 	t.Run("take slices/arrays store in new slice", func(t *testing.T) {
 		got := SumAll([]int{1, 2}, []int{2, 8})
 		want := []int{3, 10}
 
 		// cant use != qaulity operator with slices , must use '!reflect.DeepEqual'
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
+}
 
+func TestSumAllTails(t *testing.T) {
 	t.Run("sum of 'tails' of each slice", func(t *testing.T) {
 		got := SumAllTails([]int{1, 2}, []int{2, 1})
 		want := []int{2, 1}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
 
 	t.Run("sum safely empty slices", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
+}
+
+func checkSums(t testing.TB, got, want []int) {
+	t.Helper()
+	// cant use != qaulity operator with slices , must use '!reflect.DeepEqual'
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
