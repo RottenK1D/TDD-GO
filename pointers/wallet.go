8c65c97265b24d0d602879941e8a41b9f739e66d
nuker <rinards.sondors@gmail.com>
2023-04-26 16:35:51 +0100

    implement type

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,10 +1,19 @@
 package pointers
 
+import (
+	"fmt"
+)
+
 type Bitcoin int
 
 type Depositer interface {
 	Deposit(Bitcoin) Bitcoin
 }
+
+type Withdrawal interface {
+	Withdraw(Bitcoin)
+}
+
 type Balancer interface {
 	Balance() Bitcoin
 }
@@ -17,6 +26,14 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
+}
+
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -5,14 +5,29 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
+	// test deposit logic
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
+		got := wallet.Balance()
 
-	want := Bitcoin(10)
+		want := Bitcoin(10)
 
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+	// test withdraw logic
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(10)}
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+		want := Bitcoin(0)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
