06e31bc14e49664e039d8f0167c0d0ae455727d4
nuker <rinards.sondors@gmail.com>
2023-04-26 17:11:43 +0100

    refactor, add support func

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -5,29 +5,22 @@ import (
 )
 
 func TestWallet(t *testing.T) {
+	assert := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		if wallet.balance != want {
+			t.Errorf("got %s want %s", wallet.balance, want)
+		}
+	}
 	// test deposit logic
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
-
-		got := wallet.Balance()
-
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assert(t, wallet, Bitcoin(10))
 	})
 	// test withdraw logic
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(10)}
 		wallet.Withdraw(Bitcoin(10))
-
-		got := wallet.Balance()
-		want := Bitcoin(0)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assert(t, wallet, Bitcoin(0))
 	})
 }
